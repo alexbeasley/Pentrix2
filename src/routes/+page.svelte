f93b99b5c7dec81cc8dd31560438d96219d42a58
Alex Beasley <alex@nolaedu.com>
2024-09-30 13:18:22 -0500

    update remove cdn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
 
-    <h1>Play Pentrix Game test</h1>
+    <h1>Play Pentrix Game</h1>
     <!-- Embed the SWF game -->
     <embed src="pentrix.swf" width="800" height="600" type="application/x-shockwave-flash">
